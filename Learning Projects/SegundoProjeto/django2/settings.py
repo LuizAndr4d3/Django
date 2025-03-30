ac30237a1cc747f08e04d388737ad7b27c4ed92c
Luiz-Gustavo-GIT <luizg.andrade7@gmail.com>
2025-03-29 22:33:51 -0300

    setings update

--- Learning Projects/SegundoProjeto/django2/settings.py
+++ Learning Projects/SegundoProjeto/django2/settings.py
@@ -12,12 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
-#usando PostgreSQL com Heroku.
-DATABASES = {
-    'default': dj_database_url.config()
-}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
